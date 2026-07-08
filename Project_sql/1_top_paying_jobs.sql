989434d9ba67f4d2012b565980579c546661f5e5
Kumud Yadav <kumudyadav847@gmail.com>
2026-07-08 11:25:47 +0530

    Query 1

---
+++ Project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,25 @@
+/*
+Question: What are the top paying jobs ?
+- Identify the top Business Analyst jobs available remotely.
+- Focus on job postings with specified salaries (remove null).
+- Why? Higlight the top paying opportunities for Business Analysts
+*/
+
+SELECT
+    job_id,
+    job_title,
+    name AS company_name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Business Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10
